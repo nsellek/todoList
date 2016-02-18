eaf69e0d40d0084d7a6acf646de024c6c7e27ff0
nsellek <nick@seansellek.com>
2016-02-18 10:28:41 -0500

    Added a create method

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,17 @@
 class TodoController < ApplicationController
   def home
+  	@todos = Todo.all
+  	@todo = Todo.new
   end
+
+  def create
+  	@todo = Todo.new(todo_params)
+  	@todo.save
+  	render json: @todo.to_json
+  end
+
+  private
+  	def todo_params
+  		params.permit(:name)
+  	end
 end
