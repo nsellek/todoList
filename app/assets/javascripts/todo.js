d37f127da13c8c2256e761e3495c10438b86915c
nsellek <nick@seansellek.com>
2016-02-18 15:31:05 -0500

    Allows items to be added to list without reload

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -1,7 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
+function puts(arg){
+	console.log(arg);
+}
 
-$(document).on("ready", function(){
-	console.log('document done loading');
+
+$(document).on("ready page:load", function(){
+	puts('document done loading');
+
+	$('.new_todo').on('ajax:success', newTodo);
+
+	function newTodo() {
+		$('#todo').append('<li>' + arguments[1].name + '</li>');
+		$('#name').val('');
+	}
 });
